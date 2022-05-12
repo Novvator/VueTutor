a328c4ec3860834657b2a0375e6837b36c9cb42d
Novvator <stamatis.sellas@yahoo.com>
2022-05-12 13:27:13 +0300

    added 2.Basics

--- 1.First App/gs-01-starting-project/app.js
+++ 1.First App/gs-01-starting-project/app.js
@@ -1,24 +1,21 @@
-Vue.createApp ({
+Vue.createApp({
   data() {
     return {
       goals: [],
-      enteredValue: ''
+      enteredValue: "",
     };
   },
   methods: {
     addGoal() {
       this.goals.push(this.enteredValue);
-      this.enteredValue = '';
-    }
-  }
-}).mount('#app');
-
-
+      this.enteredValue = "";
+    },
+  },
+}).mount("#app");
 
 // const buttonEl = document.querySelector('button');
 // const inputEl = document.querySelector('input');
 // const listEl = document.querySelector('ul');
-
 
 // function addGoal() {
 //   const enteredValue = inputEl.value;
@@ -29,4 +26,3 @@ Vue.createApp ({
 // }
 
 // buttonEl.addEventListener('click', addGoal);
-
