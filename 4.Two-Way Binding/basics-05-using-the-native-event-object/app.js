063a8192f2ab15ecc048381350972ac70a8c6b1e
Novvator <stamatis.sellas@yahoo.com>
2022-05-17 13:22:50 +0300

    completed 4.

--- 4.Two-Way Binding/basics-05-using-the-native-event-object/app.js
+++ 4.Two-Way Binding/basics-05-using-the-native-event-object/app.js
@@ -2,19 +2,57 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
-      name: ''
+      name: '',
+      // fullname: '',
+      lastname: ''
     };
   },
+  watch: {
+    counter(value ) {
+      if( value > 50) {
+        const that = this;
+        setTimeout(function () {
+          that.counter = 0;
+        },2000);
+
+      }
+    }
+    // name(value) {
+    //   if(value === ''){
+    //     this.fullname = '';
+    //   } else {
+    //     this.fullname = value + ' ' + this.lastname;
+    //   }
+
+    // },
+    // lastname(value) {
+    //   if(value === '') {
+    //     this.fullname = ''
+    //   } else {
+    //     this.fullname = this.name + ' ' + value;
+    //   }
+    // }
+  },
+  computed: {
+    fullname() {
+      console.log('running..');
+      if(this.name === '' || this.lastname === '') {
+        return '';
+      }
+      return this.name + ' ' + this.lastname;
+    }
+  },
   methods: {
-    setName(event, lastName) {
-      this.name = event.target.value + ' ' + lastName;
-    },
     add(num) {
       this.counter = this.counter + num;
     },
     reduce(num) {
       this.counter = this.counter - num;
       // this.counter--;
+    },
+    resetInput() {
+      this.name = '';
+      this.lastname = '';
     }
   }
 });
